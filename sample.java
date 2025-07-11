4441ea9674ca0a26f9d4cc2ab5b0036dfb0f6653
anjan095 <anjanpadmanabhaiah@gmail.com>
2025-07-11 12:16:53 -0400

    third commit

--- sample.java
+++ sample.java
@@ -3,5 +3,6 @@ class sample {
         System.out.println("Hello, World!");
         System.out.println("This is a sample Java program.");
         System.out.println("It demonstrates basic output.");
+        System.out.println("End of the sample program.");
     }
 }
